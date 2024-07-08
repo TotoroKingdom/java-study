39152cff877e5e3dbdfde497d207e2a52ab1e73d
totoro <1026215903@qq.com>
2024-07-08 09:17:40 +0800

    DaemonThread

--- src/main/java/com/totoro/javastudy/juc/DaemonTest.java
+++ src/main/java/com/totoro/javastudy/juc/DaemonTest.java
@@ -1,7 +1,23 @@
 package com.totoro.javastudy.juc;
 
 public class DaemonTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t = new Thread(() ->{
+            while (true){
+                try{
+                    System.out.println("程序正常运行中。。。");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1000);
+        }
 
 
     }
